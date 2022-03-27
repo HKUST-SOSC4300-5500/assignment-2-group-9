35ce5210ce8299cc4c975fd86eff5cd72eb5fb57
Silvan Hofer <sm.hofer@blueiwn.ch>
2022-03-27 09:07:19 +0800

    notes

--- 0 data prep.R
+++ 0 data prep.R
@@ -1,6 +1,30 @@
+#1. load data and fix encodings ------------------------------------------------
 
-data <- read.csv("game_train.csv")
-games <- read.csv("games.csv")
+reviews <- read.csv("game_train.csv", encoding = "UTF-8")
+games <- read.csv("games.csv", encoding = "UTF-8")
+
+#2. Combine data----------------------------------------------------------------
+
+data <- merge(reviews, games, by.x = "title", by.y = "title", all.x = T)
+rm(reviews, games)
+
+#3. Clean data
+
+data$is.early <- ifelse(grepl("Early Access Review", data$user_review), 1, 0)
+data$user_review <- gsub("Early Access Review","", data$user_review)
+
+data$received.free <- ifelse(grepl("Product received for free", data$user_review), 1, 0)
+data$user_review <- gsub("Product received for free","", data$user_review)
+
+data$user_review <- gsub("Access Review", "", data$user_review)
+
+#
+data$user_review <- tolower(data$user_review)
+
+data$user_review <- gsub("[#&~%:*.!?,$\"/\\-]", "", data$user_review)
+data$user_review <- gsub("[0-9]", "", data$user_review)
 
 
-test <- merge(data, games, by.x = "title", by.y = "title", all.x = T)
+datacorpus <- corpus(data$user_review)
+datacorpus
+summary(datacorpus)
